8d8a46992b23bb17daf42aa11562d715b6927a11
mpetty <michael.petty@gmail.com>
2021-10-21 11:04:50 -0700

    add change/submit handler; use state for email and ref for password as examples of each method

--- src/common/ui/form/Input/index.js
+++ src/common/ui/form/Input/index.js
@@ -1,12 +1,14 @@
-const Input = props => {
+import React from "react"
+
+const Input = React.forwardRef((props, ref) => {
     const { type, label, id, ...other } = props
 
     return (
         <>
             <label htmlFor={id}>{label}:</label>
-            <input type={type} id={id} {...other}/>
+            <input type={type} id={id} {...other} ref={ref} />
         </>
     )
-}
+})
 
 export default Input
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,44 @@
+import { useState, useRef } from 'react'
+
 import { Input, Button } from '../../common/ui/form'
 
 const Login = (props) => {
+    // using state for email and ref for password
+    // to illustrate the two options 
+    const [userInput, setUserInput] = useState({email: ''})
+    const passwordRef = useRef()
+
+    const changeHandler = e => {
+        setUserInput((prevState) => {
+            return { ...prevState, [e.target.name]: e.target.value }
+        })
+    }
+
+    const submitHandler = e => {
+        e.preventDefault()
+        console.log('password: ', passwordRef.current.value)
+        passwordRef.current.value = ''
+        setUserInput({email: ''})
+    }
     return (
         <>
         <h3>Login to your Account</h3>
-        <form>
-            <Input type='email' name='email' label='Email' id='userEmail'/>
-            <Input type='password' name='password' id='userPassword' label='Password' />
+        <form onSubmit={submitHandler}>
+            <Input 
+                type='email' 
+                name='email' 
+                label='Email' 
+                id='userEmail' 
+                value={userInput.email}
+                onChange={changeHandler} 
+            />
+            <Input 
+                type='password' 
+                name='password' 
+                id='userPassword' 
+                label='Password' 
+                ref={passwordRef}
+            />
             <Button type='submit'>Login</Button>
         </form>
         </>
